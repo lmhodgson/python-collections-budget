618c207d763ed64dd0137e1aaaf1c0c08ac27817
Lucy Hodgson <32247654+lmhodgson@users.noreply.github.com>
2020-12-20 15:15:09 +0000

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -16,13 +16,13 @@ def main():
         if not (a.issubset(b) and b.issubset(a)):
             print('Sets are NOT equal by subset test')
 
-    timeit.timeit(stmt="expenses.categorize_for_loop()", setup=
+    print(timeit.timeit(stmt="expenses.categorize_for_loop()", setup=
     '''
 from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 ''',
-                  number=100000, globals=globals())
+                  number=100000, globals=globals()))
 
 
 if __name__ == "__main__":
